3d6a4b1757a876b81da38e68d5b98c5a85f5ef89
izak <maingiisaac27@gmail.com>
2026-07-14 16:34:04 +0300

    Date validation and ceompenation bug

--- src/main/java/com/riverbank/employee_management_backend/dto/employee/LeaveRequest.java
+++ src/main/java/com/riverbank/employee_management_backend/dto/employee/LeaveRequest.java
@@ -7,10 +7,20 @@ import java.time.LocalDate;
 import java.util.UUID;
 
 public record LeaveRequest(
-      @NotNull LeaveType leaveType,
-      @NotNull LocalDate startDate,
-      @NotNull LocalDate endDate,
+
+      @NotNull(message = "Leave type is required")
+      LeaveType leaveType,
+
+      @NotNull(message = "Start date is required")
+      LocalDate startDate,
+
+      @NotNull(message = "End date is required")
+      LocalDate endDate,
+
       String reason,
+
+      @NotNull(message = "Please select a cover employee")
       UUID coverEmployeeId
+
 ) {
 }
